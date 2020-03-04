6af9796a845f0cc80fcc82beec14e54a909ac047
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-03-04 08:10:46 -0700

    Add styling to gallery by addinf display flex and adding scroll on the x index

--- src/GalleryContainer.svelte
+++ src/GalleryContainer.svelte
@@ -5,7 +5,7 @@
 
 <section class='gallery'>
    {#each arts as art}
-        <section>
+        <section class='paintings'>
             <img src={art.baseimageurl} alt='paintings'/>
         </section>
    {/each}
@@ -16,5 +16,21 @@
         background-color: #FEF7DE;
         height: 100vh;
         padding: 20px;
+        display: flex;
+        /* justify-content: center; */
+        /* flex-wrap: wrap; */
+        align-items: center;
+        overflow-x: scroll;
+    }
+
+    .paintings {
+        margin: 10px;
+        padding: 10px;
+        background-color: white;
+        height: 30em;
+    }
+
+    img {
+        height: 30em;
     }
 </style>
