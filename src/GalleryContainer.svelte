1d229ec5a248f16d06d59d9567350878666742a4
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-03-04 09:00:44 -0700

    Add final styling to arts in art gallery

--- src/GalleryContainer.svelte
+++ src/GalleryContainer.svelte
@@ -9,9 +9,9 @@
         <section class='paintings'>
             <img src={art.images[0].baseimageurl} alt='paintings'/>
             <article>
-                <p class='culture'>{art.title}</p>
-                <p class='culture'>Culture: {art.culture}</p>
-                <p class='culture'>Dated: {art.dated} </p>
+                <p>{art.title}</p>
+                <p>Culture: {art.culture}</p>
+                <p >Dated: {art.dated} </p>
             </article>
         </section>
    {/each}
@@ -30,15 +30,16 @@
     }
 
     .paintings {
-        margin: 10px;
-        padding: 10px;
-        background-color: white;
+        margin: 15px;
+        padding: 15px;
+        border-radius: 5px;
+        background: linear-gradient( to left, #d6a73e 22%,#d2a559 25%, #d4bc83 40%, #e3ba62 31%,#d8af32 45%,#d09532 80%) left bottom transparent no-repeat;
         height: 30em;
     }
 
     article{
       text-align: center;
-      margin: 10px;
+      margin-top: 20px;
       padding: 10px;
       border: 1px solid black;
       background-color: white;
@@ -46,14 +47,12 @@
 
     article > p {
         margin: 10px;
+        font-size: 20px;
     }
 
     img {
         height: 30em;
-    }
-
-    .culture {
-        font-size: 20px;
+        border: 2px solid black;
     }
 
     p {
