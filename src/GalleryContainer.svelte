a5f2e463d56907df144286df47ffdb558a3a7e06
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-03-04 07:19:22 -0700

    Add background colour to gallery as well as padding and height

--- src/GalleryContainer.svelte
+++ src/GalleryContainer.svelte
@@ -2,10 +2,14 @@
   
 </script>
 
-<main>
-
-</main>
+<section class='gallery'>
+  <p> innit </p>
+</section>
 
 <style>
-	
+	.gallery {
+        background-color: #FEF7DE;
+        height: 100vh;
+        padding: 20px;
+    }
 </style>
