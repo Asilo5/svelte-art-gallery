3756f1cdef7b09ad1aedce3453e874c049a1e437
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-03-04 09:45:56 -0700

    Add next page fetch

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
 	import GalleryContainer from './GalleryContainer.svelte';
 
 	let arts = [];
+	let count = 1;
 
 	onMount( () => {
 		fetch(`https://api.harvardartmuseums.org/object?apikey=30de38c0-5cb4-11ea-9401-ed9c8b78a90d`)
@@ -11,7 +12,14 @@
 		.then(data => arts = data.records)
 		.then(error => console.log(error))
 	})
-	
+
+	const nextGallery = () => {
+		fetch(`"https://api.harvardartmuseums.org/object?apikey=30de38c0-5cb4-11ea-9401-ed9c8b78a90d&page={${count}}"`)
+		.then(res => res.json())
+		.then(data => arts = data.records)
+		.then(error => console.log(error))
+	}
+
 </script>
 
 <main>
