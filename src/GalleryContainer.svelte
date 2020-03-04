98d8e077497a12ba238a9208ddc9fe734a4bc754
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-03-04 08:52:03 -0700

    Add style to info of art

--- src/GalleryContainer.svelte
+++ src/GalleryContainer.svelte
@@ -7,10 +7,12 @@
    <p>↠</p>
    {#each arts as art}
         <section class='paintings'>
-            <p class='culture'>{art.title}</p>
             <img src={art.images[0].baseimageurl} alt='paintings'/>
-            <p class='culture'>Culture: {art.culture}</p>
-            <p class='culture'>Dated: {art.dated} </p>
+            <article>
+                <p class='culture'>{art.title}</p>
+                <p class='culture'>Culture: {art.culture}</p>
+                <p class='culture'>Dated: {art.dated} </p>
+            </article>
         </section>
    {/each}
    <p>↞</p>
@@ -32,6 +34,18 @@
         padding: 10px;
         background-color: white;
         height: 30em;
+    }
+
+    article{
+      text-align: center;
+      margin: 10px;
+      padding: 10px;
+      border: 1px solid black;
+      background-color: white;
+    }
+
+    article > p {
+        margin: 10px;
     }
 
     img {
