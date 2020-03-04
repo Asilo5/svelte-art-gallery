590b606364cf22671c77bca21a74b127ff99b3e4
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-03-03 22:16:28 -0700

    Add fetch to retrieve all current 10 paintings

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,14 @@
 <script>
-  
+  	import { onMount } from 'svelte';
+
+	let arts = [];
+
+	onMount(async () => {
+		const res = await fetch(`https://api.harvardartmuseums.org/image?apikey=30de38c0-5cb4-11ea-9401-ed9c8b78a90d`);
+		let allPaints = await res.json();
+		arts = allPaints.records;
+		console.log('arts', arts)   
+	});
 </script>
 
 <main>
